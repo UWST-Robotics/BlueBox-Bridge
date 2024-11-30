ec015a95880c301cf753e2ef5447a0f81b2f05d1
Nick Doboszenski <84477153+Waffleer@users.noreply.github.com>
2024-11-30 15:49:06 -0600

    Nano testing soon

--- README.md
+++ README.md
@@ -19,3 +19,14 @@ Helpful functions
 
 
 # Set up ser2net
+
+
+
+# Order
+    apt-get install pip -y
+    apt-get install git -y
+    git clone https://github.com/UWST-Robotics/BlueBox-Bridge.git
+
+    sudo apt install python3-fastapi -y
+
+    python3 -m uvicorn serialapi:app --host 192.168.120.28 --port 8100
--- serialapi.py
+++ serialapi.py
@@ -1,56 +1,100 @@
 from fastapi import FastAPI
+import os
+from os import listdir
+from os.path import isfile, join
+
 
 app = FastAPI()
 
 IP = "192.168.0.1"
-PORT = 8111
+PORT = 8100
 
-YAMLHEADER = """
-%YAML 1.1
----
+YAMLHEADER = "%YAML 1.1\n---\n\n"
+
+PORTS = [] # [ portNum, usbPath ]
+
+# Helper Functions
+def getSerialConfig(id: int, path: str) -> str: # Tested Working
+    # Returns config for serial device to telnet
+    res = "connection: &serialcon"+ str(id) +"\n  accepter: telnet,tcp, "+str(IP)+", "+str(PORT + id)+"\n  enable: on\n  options:\n  connector: serialdev, "+path+", local\n\n"
+    print(res)
+    return res
+def resetSer2NetService() -> str: # TODO TEST
+    #run linux command to reset ser2net.service
+    command = "systemctl restart ser2net"     
+    os.system(command)
+    return "success"
+
+def getSerialDevices() -> list[str]: #TODO TEST
+    # Get list of serial devices in folder /dev/serial
+    serialPath = "/dev/serial/by-id/"
+
+    onlyfiles = [f for f in listdir(serialPath) if isfile(join(serialPath, f))] # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+    #return onlyfiles
+    return ["/dev/serial/by-id/vex2","/dev/serial/by-id/vex0"]
 
 
-"""
 
-ports = []
-
-def getSerialConfig(id: int, path: str):
-    return f"""
-    connection: &serialcon{id}
-    accepter: telnet,tcp, {IP}, {PORT + id}
-    enable: on
-    options:
-        kickolduser: true
-    connector: serialdev, {path}, local
-    """
-
-#get path of serial devices and assign ids
-
+# Main Functions
 @app.get("/")
-def ping():
-    return {"ping": "pong"}
+def ping() :# Tested Working
+    return {"status": "pong"}
 
-@app.get("/ports") # Returns list of ports
-def getPorts():
-    return {"ports": f"{ports}"}
+@app.get("/ports")
+def getPorts(): # Tested Working
+    # Returns list of ports
+    return {"ports": PORTS}
 
-@app.get("/ports") # Returns list of ports
+@app.get("/setPorts")
 def setPorts():
-    # getSerialDevices
-    # 
-    return {"ports": f"{ports}"}
+     # Returns list of ports
+    devs = getSerialDevices()
+
+    # new yaml config
+    f = open("etc/ser2net.yaml", "w") # w will overwrite the existing file
+    f.write(YAMLHEADER) # set YAMLHEADER
+    
+    for x in range(0, len(devs)): # Tetsed Working
+        f.write(getSerialConfig(x,devs[x]))
+        PORTS.append( [PORT + x, devs[x]])
+    f.close()
+
+    resetSer2NetService()
+
+    return {"status": "success"}
 
 
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
+# Extra Get Commands
+@app.get("/ports/by-port-number/{portNum}") # Tested Working
+def read_item(portNum: int):
+    for x in PORTS:
+        if(x[0] == portNum):
+            return {"port": x[1]}
+    return {"status": "not found"}
+
+@app.get("/ports/by-usb-path/{usbPath}") # Tested Working
+def read_item(usbPath: str):
+    for x in PORTS:
+        if(x[1] == usbPath):
+            return {"port": x[0]}
+    return {"status": "not found"}
 
 
 
 
-#assign ports for each serial device
+# Test functions
+@app.get("/tests/getSerialConfig/{id}/{path}")
+def getPorts(id: int, path: str):
+    # Returns list of ports
+    return {"return": getSerialConfig(id, path)}
 
+@app.get("/tests/resetSer2NetService")
+def testResetSer2NetService():
+    # runs resetSer2NetService
+    return {"return": resetSer2NetService()}
 
-
-
+@app.get("/tests/getSerialDevices")
+def testGetSerialDevice():
+    # run getSerialDevices
+    return {"return": getSerialDevices()}
